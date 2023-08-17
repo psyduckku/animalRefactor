52f9b80b9e53fdbe414ddc40b8dae10579056ca1
pk <psyducku@naver.com>
2023-08-17 22:49:56 +0900

    최초리팩토링 프로젝트 커밋

---
+++ animals/src/test/java/com/refactor/animals/AnimalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refactor.animals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
